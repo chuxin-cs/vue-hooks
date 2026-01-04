6261c369c0c48336c6cad02df18ac00d905106ff
chuxin <3970987337@qq.com>
2026-01-04 14:36:06 +0800

    refactor: 重命名变量并添加姓名输出
    将变量a重命名为age以提高可读性，并添加姓名输出功能

--- Rust/greeting/src/main.rs
+++ Rust/greeting/src/main.rs
@@ -3,8 +3,12 @@ fn main() {
     sum();
 }
 
+
 fn sum(){
-    let a = 30;
-    // 输出 a 的值
-    println!("a = {}", a);
+    let age = 30;
+    // 输出 age 的值
+    println!("age = {}", age);
+
+    let name = "chuxin";
+    println!("姓名是 = {0}, 年龄是 = {1}", name, age);
 }
